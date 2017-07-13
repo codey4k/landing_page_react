4edb187b8e8993d4f01fc1aac4bee61625927487
codey4k <codey4k@gmail.com>
2017-07-13 19:44:57 +0000

    Updating project files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class App extends React.Component {
+    render() {
+        return(
+            <div className="app">
+                <h1>Happy hacking</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.body)
