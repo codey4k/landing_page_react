bfe8ba39850d38cbb91983a1afbeb824b32b6501
codey4k <codey4k@gmail.com>
2017-08-17 19:30:15 +0000

    change of design

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,18 +2,12 @@ import React from 'react'
 import './home.css'
 
 import Header from '../../global/header/Header.js'
-import DevSection from './home_sections/devsection/devSection.js'
-import Services from './home_sections/services/services.js'
-import Slide from './home_sections/slide/slide.js'
 
 export default class Home extends React.Component {
     render() {
         return(
             <div className="home">
                 <Header />
-                <Services />
-                <Slide />
-                <DevSection />
             </div>
         )
     }
--- src/global/header/Header.js
+++ src/global/header/Header.js
@@ -1,26 +1,11 @@
 import React from 'react'
-import { Parallax } from 'react-parallax'
 
 import './header.css'
-
-import hero from '../../assets/hero.jpg'
-
-
 
 export default class Header extends React.Component {
     render() {
         return(
             <div className="header">
-                <Parallax  bgImage={hero} 
-                           bgHeight="340px"
-                           bgWidth="100%"
-                           className="header__parallax" 
-                           strength={100}>
-                    <div className="header__welcome">
-                        <h2>Welcome to CodeY4k web site</h2>
-                        <p>Happy hacking</p>
-                    </div>
-                </Parallax>
             </div>
         )
     }
