3aff90467908107899b6c32df7f63704b53f8e3d
codey4k <codey4k@gmail.com>
2017-09-20 20:44:30 +0000

    adding new logo

--- src/global/menu_bar/navBar.js
+++ src/global/menu_bar/navBar.js
@@ -1,8 +1,10 @@
 import React from 'react'
+// import ReactSVG from 'react-svg'
 import { Link } from 'react-router-dom'
 
 import './menu.css'
 import react from './react.png'
+import logo2 from './logo2.png'
 // import codey4k from './codey4k.png'
 
 export default class Menu extends React.Component {
@@ -10,6 +12,7 @@ export default class Menu extends React.Component {
         return(
             <div className="menu">
                 <div className="menu__container">
+                    <img src={logo2} alt="logo2" className="menu__logo_y4k"/>
                     <Link to="/">
                         <img src={react} alt="img" className="menu__logo" />
                     </Link>
