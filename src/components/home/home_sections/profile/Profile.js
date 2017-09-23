aa6ffd4a90f940dc7a135d38e332ac2d9a303f0b
codey4k <codey4k@gmail.com>
2017-09-23 23:48:05 +0000

    add styles to profile section

--- src/components/home/home_sections/profile/Profile.js
+++ src/components/home/home_sections/profile/Profile.js
@@ -9,7 +9,13 @@ export default class Profile extends React.Component {
                 <div className="profile__container">
                     <div className="profile__img"></div>
                     <div className="profile__info">
-                        <h3>Y4k</h3>
+                        <div className="profile__title">
+                            <h3>Y4k</h3>
+                        </div>
+                        <div className="profile__description">
+                            <p>Developer | Designer | Freelancer | Hacker</p>
+                        </div>
+                        <div className="profile__social"></div>
                     </div>
                 </div>
             </div>
