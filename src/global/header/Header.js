14cb2a25465ff9c8a2363bda014e80f00370ab96
codey4k <codey4k@gmail.com>
2017-11-09 09:49:52 +0000

    new header style

--- src/global/header/Header.js
+++ src/global/header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import './header.css'
+import img from './img_header.jpg'
+
 
 export default class Header extends React.Component {
     render() {
         return(
             <div className="header">
+                <img src={img} alt="img-header" className="header__img"/>
             </div>
         )
     }
