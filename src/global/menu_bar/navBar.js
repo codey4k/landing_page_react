f5ce9b3f801dde2008750f3bc0cfad5ed88bccf1
codey4k <codey4k@gmail.com>
2017-11-18 21:19:30 +0000

    adding state to Menu component

--- src/global/menu_bar/navBar.js
+++ src/global/menu_bar/navBar.js
@@ -8,9 +8,40 @@ import logo2 from './logo2.png'
 // import hamburger from './hamburger_icon.svg'
 
 export default class Menu extends React.Component {
+    
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            height : 80,
+            background : 'transparent'
+        }
+        this.handlerScroll = this.handlerScroll.bind(this)
+    }
+    
+    componentDidMount() {
+        window.addEventListener('scroll', this.handlerScroll)
+    }
+    
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handlerScroll)
+    }
+    
+    handlerScroll() {
+        let scroll = document.documentElement.scrollTop || document.body.scrollTop
+        
+        scroll >= 300 ? 
+            this.setState({height : 60, background : '#333'}) :
+            this.setState({height : 80, background : 'transparent'})
+    }
+    
     render() {
+        const styles = {
+            height : this.state.height,
+            background : this.state.background
+        }
         return(
-            <div className="menu">
+            <div className="menu" style={styles}>
                 <div className="menu__container">
                     <img src={logo2} alt="logo2" className="menu__logo_y4k"/>
                     <Link to="/">
