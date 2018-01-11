f5b680c9a263a6250e51a2b3ececea90545c1a36
codey4k <codey4k@gmail.com>
2018-01-11 22:55:00 +0000

    11/01/2018

--- src/global/header/Header.js
+++ src/global/header/Header.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 import './header.css'
 import img from './img_header.jpg'
+import headerimg from './header01.jpg'
 
 
 export default class Header extends React.Component {
     render() {
         return(
             <div className="header">
-                <img src={img} alt="img-header" className="header__img"/>
+                <img src={headerimg} alt="img-header" className="header__img"/>
             </div>
         )
     }
