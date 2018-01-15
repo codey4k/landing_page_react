dadacd1ecb026ed344309436b0a1cd5a19120e17
codey4k <codey4k@gmail.com>
2018-01-15 23:18:56 +0000

    new component info

---
+++ src/components/home/home_sections/info/Info.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Info extends React.Component {
+    render(){
+        return(
+            <section className="Info">
+                <div className="info__container">
+                    <h2 className="info__title"></h2>
+                    <p className="info__content">
+                    fdij fdi jre erewr qweewqrwe qweqw wqew qeqwe
+                    etret werwer asd asd rt asd wert ewr 
+                    </p>
+                </div>
+            </section>
+        )
+    }
+}
