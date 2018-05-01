b35be31de72b4521a246678c8770a6bf11013054
codey4k <codey4k@gmail.com>
2018-05-01 22:17:16 +0000

    defining page 404

--- src/components/page404/Page404.js
+++ src/components/page404/Page404.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import './page404.css'
+
 export default class Page404 extends React.Component {
     render() {
         return(
-            <div>
-                <h1>Page 404</h1>
+            <div className="page404">
+                <div className="page404__container">
+                    <h1>Page 404</h1>
+                    <p>Page not found!</p>
+                </div>
             </div>
         )
     }
